33f2a60ce4c68350632ccacd327710ad70724e42
rulo <nahuel.developer@icloud.com>
2022-05-06 17:33:29 -0300

    feat(03-tdd-app-setup): Added basic tests for AppModel state.

--- 03-tdd-app-setup/projects/starter/FitNess/FitNessTests/AppModelTests.swift
+++ 03-tdd-app-setup/projects/starter/FitNess/FitNessTests/AppModelTests.swift
@@ -31,15 +31,34 @@
 /// THE SOFTWARE.
 
 import XCTest
+import FitNess
 
 class AppModelTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+  override func setUpWithError() throws {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-    
+  override func tearDownWithError() throws {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
+  
+  func testAppModel_whenInitialized_isInNotStartedState() {
+    let sut = AppModel()
+    let initialState = sut.appState
+    XCTAssertEqual(initialState, AppState.notStarted)
+  }
+  
+  func testAppModel_whenStarted_isInInProgressState() {
+    // 1 given app in not started
+    let sut = AppModel()
+
+    // 2 when started
+    sut.start()
+
+    // 3 then it is in inProgress
+    let observedState = sut.appState
+    XCTAssertEqual(observedState, .inProgress)
+  }
+  
 }
