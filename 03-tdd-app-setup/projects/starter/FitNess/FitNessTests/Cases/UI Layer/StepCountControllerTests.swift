5c8af7aa7276c36b1aa89a9ae23a3d64b8eafdf3
rulo <nahuel.developer@icloud.com>
2022-06-21 16:27:32 -0300

    feat(03-tdd-app-setup): Added tests for StepCountController. We can write valuable unit tests by separating the state-controlling logic from the view hierarchy.

--- 03-tdd-app-setup/projects/starter/FitNess/FitNessTests/Cases/UI Layer/StepCountControllerTests.swift
+++ 03-tdd-app-setup/projects/starter/FitNess/FitNessTests/Cases/UI Layer/StepCountControllerTests.swift
@@ -31,14 +31,50 @@
 /// THE SOFTWARE.
 
 import XCTest
+@testable import FitNess
 
 class StepCountControllerTests: XCTestCase {
-
+  
+  var sut: StepCountController!
+  
   override func setUpWithError() throws {
-    
+    try super.setUpWithError()
+    sut = StepCountController()
   }
 
   override func tearDownWithError() throws {
-    
+    sut = nil
+    try super.tearDownWithError()
   }
+  
+  // MARK: - Initial State
+
+  func testController_whenCreated_buttonLabelIsStart() {
+    // given
+    sut.viewDidLoad()
+    
+    let text = sut.startButton.title(for: .normal)
+    XCTAssertEqual(text, AppState.notStarted.nextStateButtonLabel)
+  }
+
+  // MARK: - In Progress
+  
+  func testController_whenStartTapped_appIsInProgress() {
+    // when
+    sut.startStopPause(nil)
+
+    // then
+    let state = AppModel.instance.appState
+    XCTAssertEqual(state, AppState.inProgress)
+  }
+  
+  func testController_whenStartTapped_buttonLabelIsPause() {
+    // when
+    sut.startStopPause(nil)
+
+    // then
+    let text = sut.startButton.title(for: .normal)
+    XCTAssertEqual(text, AppState.inProgress.nextStateButtonLabel)
+  }
+
 }
