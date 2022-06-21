62c0617368e6e41bd6c4a5eb24a344a38ced2ec4
rulo <nahuel.developer@icloud.com>
2022-06-21 16:34:36 -0300

    feat(03-tdd-app-setup): Small refactor on StepCountController.

--- 03-tdd-app-setup/projects/starter/FitNess/FitNess/UI Layer/StepCountController.swift
+++ 03-tdd-app-setup/projects/starter/FitNess/FitNess/UI Layer/StepCountController.swift
@@ -67,13 +67,16 @@ class StepCountController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    let title = AppState.notStarted.nextStateButtonLabel
-    startButton.setTitle(title, for: .normal)
+    updateButton()
   }
 
   @IBAction func startStopPause(_ sender: Any?) {
     AppModel.instance.start()
     
+    updateButton()
+  }
+  
+  private func updateButton() {
     let title = AppModel.instance.appState.nextStateButtonLabel
     startButton.setTitle(title, for: .normal)
   }
